2c17cd81ff532ec7f9dea450676a7545bd32dc3c
Andrew Henshaw <andrew.henshaw@gtri.gatech.edu>
2024-01-31 12:50:34 -0500

    start of PDF generation

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,63 @@ use anyhow::Result;
 use chrono::naive::NaiveDate;
 use chrono::Duration;
 use icalendar::parser;
+use printpdf::path::PaintMode;
+use printpdf::{Mm, PdfDocument, Pt, Rect};
 use std::collections::HashMap;
-use std::path::{Path, PathBuf};
+// use std::fmt::write;
+use std::fs::File;
+use std::io::BufWriter;
+use std::path::Path;
+use std::path::PathBuf;
+
+fn main() {
+    // if let Ok(events) = get_events(&PathBuf::from("data/KHW Travel Calendar.ics")) {
+    //     let days = populate_days(&events);
+    //     for (day, events) in days {
+    //         println!("{} {:?}", day, events);
+    //     }
+    // }
+    write_to_pdf(&PathBuf::from("test.pdf"));
+}
 
 #[derive(Debug)]
-struct Event {
+pub struct Event {
     start: NaiveDate,
     end: NaiveDate,
     summary: String,
 }
 
-fn get_events(calend_datear_file_path: &Path) -> Result<Vec<Event>> {
+const COLS: usize = 3;
+const WIDTH: f32 = 612.;
+const HEIGHT: f32 = 792.;
+
+fn write_to_pdf(outpath: &Path) {
+    let (doc, page1, layer1) = PdfDocument::new(
+        "Calendar".to_string(),
+        Pt(WIDTH).into(),
+        Pt(HEIGHT).into(),
+        "Layer 1",
+    );
+    let canvas = doc.get_page(page1).get_layer(layer1);
+    canvas.set_outline_thickness(0.05);
+    for _col in 0..COLS {
+        let rect = Rect::new(
+            Pt(20.).into(),
+            Pt(20.).into(),
+            Pt(100.).into(),
+            Pt(100.).into(),
+        )
+        .with_mode(PaintMode::Stroke);
+        canvas.add_rect(rect);
+    }
+
+    doc.save(&mut BufWriter::new(File::create(outpath).unwrap()))
+        .unwrap();
+}
+
+pub fn get_events(calendar_file_path: &Path) -> Result<Vec<Event>> {
     let mut events = vec![];
-    let data = std::fs::read_to_string(calend_datear_file_path)?;
+    let data = std::fs::read_to_string(calendar_file_path)?;
     let data = parser::unfold(&data);
 
     let components = parser::read_calendar_simple(&data).unwrap();
@@ -48,7 +92,7 @@ fn get_events(calend_datear_file_path: &Path) -> Result<Vec<Event>> {
     Ok(events)
 }
 
-fn populate_days(events: &Vec<Event>) -> HashMap<NaiveDate, Vec<String>> {
+pub fn populate_days(events: &Vec<Event>) -> HashMap<NaiveDate, Vec<String>> {
     let mut days = HashMap::new();
     for event in events {
         if !event.summary.starts_with("Canceled: ") {
@@ -62,15 +106,4 @@ fn populate_days(events: &Vec<Event>) -> HashMap<NaiveDate, Vec<String>> {
         }
     }
     days
-}
-
-fn main() {
-    if let Ok(mut events) = get_events(&PathBuf::from(
-        "C:/Users/ah6/Documents/Henshaw Andrew Calendar.ics",
-    )) {
-        let days = populate_days(&events);
-        for (day, events) in days {
-            println!("{} {:?}", day, events);
-        }
-    }
 }
