c8ef1ec76d4f0b425bfcb0c0da7153f9b569778b
Andrew Henshaw <andrew.henshaw@gtri.gatech.edu>
2024-02-01 16:17:43 -0500

    added month names

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use chrono::naive::NaiveDate;
 use chrono::Duration;
 use icalendar::parser;
 use printpdf::path::PaintMode;
-use printpdf::{Color, PdfDocument, Pt, Rect, Rgb};
+use printpdf::*;
 use std::collections::HashMap;
 // use std::fmt::write;
 use std::fs::File;
@@ -35,71 +35,122 @@ const GUTTER: f32 = 10.;
 const MARGIN: f32 = 36.;
 const MONTH_WIDTH: f32 = 12.;
 const DAY_WIDTH: f32 = 10.;
+const TITLE_HEIGHT: f32 = 24.;
 // const LINE_GAP: f32 = 4.0;
-const ROW: f32 = (HEIGHT - MARGIN * 2.0) / (12.0 * 31.0 / COLS as f32);
+const ROW: f32 = (HEIGHT - TITLE_HEIGHT - MARGIN * 2.0) / (12.0 * 31.0 / COLS as f32);
 const CW: f32 = (WIDTH - MARGIN * 2.0 - GUTTER * (COLS as f32 - 1.0)) / COLS as f32;
 
 fn write_to_pdf(outpath: &Path) {
-    let (doc, page1, layer1) = PdfDocument::new(
+    let (mut doc, page1, layer1) = PdfDocument::new(
         "Calendar".to_string(),
         Pt(WIDTH).into(),
         Pt(HEIGHT).into(),
         "Layer 1",
     );
+    doc = doc.with_conformance(PdfConformance::Custom(CustomPdfConformance {
+        requires_icc_profile: false,
+        requires_xmp_metadata: false,
+        ..Default::default()
+    }));
     let font_file = File::open("assets/fonts/DejaVuSans.ttf").unwrap();
     let font = doc.add_external_font(font_file).unwrap();
 
     let canvas = doc.get_page(page1).get_layer(layer1);
-    canvas.set_outline_thickness(0.01);
-    canvas.set_outline_color(Color::Rgb(Rgb::new(0.8, 0.8, 0.8, None)));
+    canvas.set_outline_thickness(0.0);
+    canvas.set_outline_color(Color::Rgb(Rgb::new(0.9, 0.9, 0.9, None)));
 
+    let year = 2024;
     let mut left = MARGIN;
     let mut month = 1;
+    let mut shade = false;
     for _ in 0..COLS {
-        let mut bottom = HEIGHT - MARGIN - ROW;
-        for _ in 1..=12 / COLS {
+        let mut bottom = HEIGHT - MARGIN - ROW - TITLE_HEIGHT;
+        let months_in_col = 12 / COLS;
+        for _ in 0..months_in_col {
+            let month_start = bottom + ROW;
+            for day in 1..=31 {
+                // check if current day okay
+                if let Some(today) = NaiveDate::from_ymd_opt(year, month, day) {
+                    let day_of_week = today.format("%a").to_string();
+                    if day_of_week == "Mon" {
+                        shade = !shade
+                    }
+
+                    // text
+                    canvas.add_rect(
+                        Rect::new(
+                            Pt(left + MONTH_WIDTH + DAY_WIDTH).into(),
+                            Pt(bottom).into(),
+                            Pt(left + CW).into(),
+                            Pt(bottom + ROW).into(),
+                        )
+                        .with_mode(PaintMode::Stroke),
+                    );
+                    // day number
+                    canvas.set_fill_color(Color::Rgb(Rgb::new(0.9, 0.9, 0.9, None)));
+                    canvas.add_rect(
+                        Rect::new(
+                            Pt(left + MONTH_WIDTH).into(),
+                            Pt(bottom).into(),
+                            Pt(left + MONTH_WIDTH + DAY_WIDTH).into(),
+                            Pt(bottom + ROW).into(),
+                        )
+                        .with_mode(if shade {
+                            PaintMode::FillStroke
+                        } else {
+                            PaintMode::Stroke
+                        }),
+                    );
+                    canvas.add_rect(
+                        Rect::new(
+                            Pt(left + MONTH_WIDTH + DAY_WIDTH).into(),
+                            Pt(bottom).into(),
+                            Pt(left + MONTH_WIDTH + 2.0 * DAY_WIDTH).into(),
+                            Pt(bottom + ROW).into(),
+                        )
+                        .with_mode(if shade {
+                            PaintMode::FillStroke
+                        } else {
+                            PaintMode::Stroke
+                        }),
+                    );
+                    canvas.set_fill_color(Color::Rgb(Rgb::new(0., 0., 0., None)));
+
+                    canvas.use_text(
+                        &format!("{}", day),
+                        6.0,
+                        Pt(left + MONTH_WIDTH).into(),
+                        Pt(bottom + 2.0).into(),
+                        &font,
+                    );
+
+                    canvas.use_text(
+                        &format!("{}", day_of_week.chars().next().unwrap()),
+                        6.0,
+                        Pt(left + MONTH_WIDTH + DAY_WIDTH).into(),
+                        Pt(bottom + 2.0).into(),
+                        &font,
+                    );
+
+                    bottom -= ROW;
+                } else {
+                    break;
+                }
+            }
             // month
+            canvas.save_graphics_state();
+            canvas.set_outline_color(Color::Rgb(Rgb::new(0.4, 0.4, 0.4, None)));
             canvas.add_rect(
                 Rect::new(
                     Pt(left).into(),
-                    Pt(bottom - (30. * ROW)).into(),
-                    Pt(left + MONTH_WIDTH).into(),
                     Pt(bottom + ROW).into(),
+                    Pt(left + CW).into(),
+                    Pt(month_start).into(),
                 )
                 .with_mode(PaintMode::Stroke),
             );
-
-            for day in 1..=31 {
-                // text
-                canvas.add_rect(
-                    Rect::new(
-                        Pt(left + MONTH_WIDTH + DAY_WIDTH).into(),
-                        Pt(bottom).into(),
-                        Pt(left + CW).into(),
-                        Pt(bottom + ROW).into(),
-                    )
-                    .with_mode(PaintMode::Stroke),
-                );
-                // day number
-                canvas.add_rect(
-                    Rect::new(
-                        Pt(left + MONTH_WIDTH).into(),
-                        Pt(bottom).into(),
-                        Pt(left + MONTH_WIDTH + DAY_WIDTH).into(),
-                        Pt(bottom + ROW).into(),
-                    )
-                    .with_mode(PaintMode::Stroke),
-                );
-                let text = format!("{}", day);
-                canvas.use_text(
-                    &text,
-                    6.0,
-                    Pt(left + MONTH_WIDTH).into(),
-                    Pt(bottom + 2.0).into(),
-                    &font,
-                );
-                bottom -= ROW;
-            }
+            canvas.restore_graphics_state();
+            write_month(&canvas, month - 1, left, month_start, bottom + ROW, &font);
             month += 1;
         }
         left += CW + GUTTER;
@@ -109,6 +160,43 @@ fn write_to_pdf(outpath: &Path) {
         .unwrap();
 }
 
+pub fn write_month(
+    canvas: &PdfLayerReference,
+    index: u32,
+    x: f32,
+    top: f32,
+    bottom: f32,
+    font: &IndirectFontRef,
+) {
+    let name = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ][index as usize]
+        .to_lowercase();
+    const MONTH_FONT_SIZE: f32 = 11.;
+    let start = (top + bottom) / 2. + (name.len() as f32) * MONTH_FONT_SIZE / 2.;
+    dbg!((top, bottom, &start));
+
+    canvas.begin_text_section();
+    canvas.set_font(&font, MONTH_FONT_SIZE);
+    canvas.set_line_height(MONTH_FONT_SIZE);
+    canvas.set_text_cursor(Pt(x).into(), Pt(start).into());
+    for c in name.chars() {
+        canvas.write_text(format!("{c}"), &font);
+        canvas.add_line_break();
+    }
+    canvas.end_text_section();
+}
 pub fn get_events(calendar_file_path: &Path) -> Result<Vec<Event>> {
     let mut events = vec![];
     let data = std::fs::read_to_string(calendar_file_path)?;
