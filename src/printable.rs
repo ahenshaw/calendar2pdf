2476ccaec80ee2f2f9e7abbff79437f9d34952b7
Henshaw, Andrew <andrew.henshaw@gtri.gatech.edu>
2024-02-27 15:09:49 -0500

    added day countdown

--- src/printable.rs
+++ src/printable.rs
@@ -171,7 +171,7 @@ pub fn base_calendar(
     );
     canvas.use_text(
         chrono::Local::now()
-            .format("Generated on %Y-%m-%d  %H:%M:%S")
+            .format("Generated: %Y-%m-%d  %H:%M:%S")
             .to_string(),
         8.,
         Pt(MARGIN).into(),
